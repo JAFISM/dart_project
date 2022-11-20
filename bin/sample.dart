6a4181d29e824ca83bce75bd6eff0c3a1ec60668
Jafis <jafism2000@gmail.com>
2022-11-20 08:12:33 +0530

    commit

---
+++ bin/sample.dart
@@ -0,0 +1,8 @@
+//entry point of the program => main function
+//structure of main function => returntype functionname(){
+// code to be executed
+// }
+void main(){
+  print("welcome to Luminar technolab");
+
+}
