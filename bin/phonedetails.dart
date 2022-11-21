405da839b584acf4fbcc80301aa864b026b0e609
Jafis <jafism2000@gmail.com>
2022-11-21 13:22:26 +0530

    commit

---
+++ bin/phonedetails.dart
@@ -0,0 +1,15 @@
+void main(){
+  print("phone details");
+  String mobname="motorola G31";
+  int price=12000;
+  int Ram=4;
+  int storage=64;
+  String os="Android 12";
+
+  print("mob name=$mobname");
+  print('price=$price');
+  print('Ram=$Ram');
+  print('Storage=$storage');
+  print('os=$os');
+
+}
---
+++ bin/variable.dart
@@ -0,0 +1,12 @@
+void main(){
+  print("my details");//direct string details
+  String name='Jafis';
+  int age=22;
+  double markperc=7.6;
+  print('name :$name');
+  print("i'm  $age  yrs old");
+  print('mark percentage :$markperc');
+
+
+
+}
