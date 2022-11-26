e57ed8168c37e5a93af66574486f66738d8405dc
Jafis <jafism2000@gmail.com>
2022-11-26 09:53:56 +0530

    commit

---
+++ bin/Switch-case.dart
@@ -0,0 +1,25 @@
+void main(){
+  var shirtsize='L';
+
+  switch(shirtsize){
+    case 'S':{
+      print('small size');
+      break;
+    }
+    case 'M':{
+      print('medium size');
+      break;
+    }
+    case 'L':{
+      print('large size');
+      break;
+    }
+    case 'XL':{
+      print('extra large size');
+      break;
+    }
+    default:{
+      print("none of these");
+    }
+  }
+}
