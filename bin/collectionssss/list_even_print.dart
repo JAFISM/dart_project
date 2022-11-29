d5fe7e81d0892c15f7c3180518d6a3ce46677391
Jafis <jafism2000@gmail.com>
2022-11-29 09:27:02 +0530

    commit

--- bin/collectionssss/list_even_print.dart
+++ bin/collectionssss/list_even_print.dart
@@ -1,7 +1,7 @@
 void main(){
-  var mylist=[1,2,3,4,5,6,7,8,9,10];
+  var mylist=[1,2,3,4,5,6,7,8,9,10,11,12,13];
   for(int index=0;index<mylist.length;index++){
-    if(index%2==0){
+    if(index%2!=0){
       print(mylist[index]);
     }
   }
---
+++ bin/multiplication_table_of_any_number.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main(){
+  print("enter a number");
+  int? j=int.parse(stdin.readLineSync()!);
+  int product=1;
+  for(int i=1;i<=10;i++){
+    product=i*j;
+    print("$i X $j=$product");
+  }
+}
