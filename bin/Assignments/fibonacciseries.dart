cbeda03fa038bdb72115a4b8385df9132a1972c9
Jafis <jafism2000@gmail.com>
2022-11-30 13:37:37 +0530

    commit

--- bin/Assignments/fibonacciseries.dart
+++ bin/Assignments/fibonacciseries.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 void main(){
   ///0 1 1 2 3 5 8 13....
-  int num=10,f1=0,s2=1;
+  print("enter a number");
+  int num=int.parse(stdin.readLineSync()!),
+      f1=0,s2=1;
   for(int i=1;i<num;i++){
     print("$f1");
     int nxt=f1+s2;
